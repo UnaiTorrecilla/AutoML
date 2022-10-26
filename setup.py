9cd65692b974196ace0dfc73cb89547f87b03f1e
Unai Torrecilla <unaitorrecillalopez@edp.com>
2022-10-26 16:48:58 +0200

    Corrected __init__ and changed version

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ long_description = (this_directory / "README.md").read_text()
 setup(
   name = 'mlforall',         # How you named your package folder (MyLib)
   packages = ['mlforall'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.3.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Library that easily allows to create machine learning progress for more unexperiencied programmers.',   # Give a short description about your library
   author = 'Unai Torrecilla',                   # Type in your name
   author_email = 'unai.torrecilla@alumni.mondragon.edu',      # Type in your E-Mail
   url = 'https://github.com/UnaiTorrecilla/MLForAll',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/UnaiTorrecilla/MLForAll/archive/refs/tags/v_03.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/UnaiTorrecilla/MLForAll/archive/refs/tags/v_031.tar.gz',    # I explain this later on
   long_description = long_description,
   long_description_content_type = 'text/markdown',
   keywords = ['Machine learning', 'Easy to use'],   # Keywords that define your package best
